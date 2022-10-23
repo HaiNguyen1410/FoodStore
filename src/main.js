74a960b2c226cc459f1d06632baf2b02bdad11f2
Hai1912 <103093087+Hai1912@users.noreply.github.com>
2022-10-24 00:46:54 +0700

    Fix Bug

--- src/main.js
+++ src/main.js
@@ -1,21 +1,47 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router/index.js"
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-import "./assets/css/style.css"
+import Vue from "vue";
+
+import App from "./App.vue";
+import router from "./router";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import "./assets/css/style.css";
+import "./assets/css/responsive.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { CarouselPlugin } from "bootstrap-vue";
 import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+import Vuex from 'vuex'
 
+library.add(faUserSecret);
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
+Vue.use(CarouselPlugin);
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state(){
+    return {
+      cart:[]
+    }
+  }
+})
 
 new Vue({
-  render: (h) => h(App),
+  router,
   vuetify,
-  router
+  render: (h) => h(App),
+  store
 }).$mount("#app");
